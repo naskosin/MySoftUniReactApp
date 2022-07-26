547fe433d6a4179f1a60efe18cea636c75924941
Atanas Sinapov <sluckata@yahoo.com>
2022-07-26 21:22:55 +0300

    updating fontawesome

--- myproject/src/components/PageNotFound/PageNotFound.js
+++ myproject/src/components/PageNotFound/PageNotFound.js
@@ -5,7 +5,7 @@ const PageNotFound = ()=>{
  <>
       <div className={styles.mainbox}>
           <div className={styles.err}>4</div>
-          <div className={styles.err}>0</div>
+          <div className={styles.err1}> <i class="far fas fa-spinner fa-pulse"></i></div>
           <div className={styles.err2}>4</div>
           <div className={styles.msg}>Ooops! Page Not Found<p>Let's go <a href="#">home</a> and try from there.</p>
           </div>
