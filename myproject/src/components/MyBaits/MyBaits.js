102115cb959d477cf6110c5aad766c881c5d98df
Atanas Sinapov <sluckata@yahoo.com>
2022-07-28 17:37:35 +0300

    Update

--- myproject/src/components/MyBaits/MyBaits.js
+++ myproject/src/components/MyBaits/MyBaits.js
@@ -1,43 +1,50 @@
-
-import {Link} from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import MyBaitsCard from './MyBaitsCard/MyBaitsCard';
-import styles from './MyBaits.module.css';
-import * as baitServices from '../../services/baitService';
-import { useAuthContext} from '../../contexts/AuthContext';
-import { useContext } from 'react';
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import MyBaitsCard from "./MyBaitsCard/MyBaitsCard";
+import styles from "./MyBaits.module.css";
+import * as baitServices from "../../services/baitService";
+import { useAuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
 
 const MyBaits = () => {
-    const {userInfo} = useAuthContext();
-    const [baits, setBaits] = useState([]);
-    
-    useEffect(()=>{
-        baitServices.getAllBaits().then(data=>{let mycatches=data.filter((x)=>x._ownerId===userInfo._id);
-            setBaits(mycatches)})
-    },[])
+  const { userInfo } = useAuthContext();
+  const [baits, setBaits] = useState([]);
 
-    
-    return (
+  useEffect(() => {
+    baitServices.getAllBaits().then((data) => {
+      let mycatches = data.filter((x) => x._ownerId === userInfo._id);
+      setBaits(mycatches);
+    });
+  }, []);
 
+  return (
+    <section className={styles.my_catches}>
+      <div className={styles.article__header__img__wrapper}>
+        <img
+          className={styles.article__header__img}
+          src="assets/05.jpg"
+          alt="No picture yet"
+        />
+      </div>
+      <p className={styles.user}>Catcher: {userInfo.email}</p>
 
-
-
-        <section className={styles.my_catches}> 
-        <div className={styles.article__header__img__wrapper}>
-   <img className={styles.article__header__img} src="assets/fisherman-smiling-holding-tuna-vector-fisherman-smiling-holding-tuna-138952859.jpg" alt="No picture yet" /></div>
-   <p className={styles.user}>Catcher: {userInfo.email}</p>
-   
-  {baits.length == 0 ?<div className={styles.nocatches}> <p>Oooops! no catches, change the bait!</p></div>: 
-   <div className={styles.catches}>
-   <p>My catches gallery:</p>
-   <ul >
-       {baits.map((bait,index)=><MyBaitsCard key={bait._id} bait ={bait} index={index}/>
-       )}
-  </ul>
-   </div>
-   }
-   </section>
- )
-}
+      {baits.length == 0 ? (
+        <div className={styles.nocatches}>
+          {" "}
+          <p>Oooops! no catches, change the bait!</p>
+        </div>
+      ) : (
+        <div className={styles.catches}>
+          <p>My catches gallery:</p>
+          <ul className={styles.mybaits__ul}>
+            {baits.map((bait, index) => (
+              <MyBaitsCard key={bait._id} bait={bait} index={index} />
+            ))}
+          </ul>
+        </div>
+      )}
+    </section>
+  );
+};
 export default MyBaits;
