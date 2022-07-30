2f157e40be00f0550b1579c64645e801397d3025
Atanas Sinapov <sluckata@yahoo.com>
2022-07-30 10:46:26 +0300

    creating NotifyContext for notifications

---
+++ myproject/src/contexts/NotifyContext.js
@@ -0,0 +1,39 @@
+import { createContext } from "react";
+import { useContext } from 'react';
+import { useState } from "react";
+
+
+export const NotifyContext = createContext();
+
+
+
+
+
+
+
+  
+
+export const NotifyProvider = ({children}) =>{
+    const [errorNotification, setErrorNotification] = useState();
+
+    
+    const notification=(error)=>{
+        setErrorNotification(error)
+        setTimeout(()=>
+        setErrorNotification('')
+      , 4000)
+    
+    }
+    
+
+
+     return(
+         <NotifyContext.Provider value={{errorNotification, notification }}>
+             {children}
+         </NotifyContext.Provider>
+     )
+};
+ export const notifyContext=()=>{
+     const notifyState = useContext(NotifyContext);
+return notifyState;
+ }
